69e9b57adc5b966d00159842edab2e50c0fad2c8
Aditya-1406 <gargaditya674@gmail.com>
2025-12-12 09:56:03 +0530

    Adding the trace viewer to html

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot:'on',
     testIdAttribute : "data-date",
-    trace: 'on-first-retry',
+    trace: 'on',
     headless: false, 
     launchOptions: { slowMo: 300 },
     
--- tests/chapter1/1_First_Test.spec.ts
+++ tests/chapter1/1_First_Test.spec.ts
@@ -1,6 +1,7 @@
 import {test,expect} from '@playwright/test'
 
 test('Verifying the Navigation', async({page})=>{
+    console.log("Hello world")
 
     await page.goto("https://practice.expandtesting.com/")
     await page.getByRole('textbox', { name: 'Search an example...' }).fill('test');
