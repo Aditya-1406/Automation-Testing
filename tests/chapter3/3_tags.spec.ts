33d85f325be3dad9f3b39a8862234d2f774bcf0d
Aditya-1406 <gargaditya674@gmail.com>
2025-12-18 16:48:23 +0530

    taggin the testcase

---
+++ tests/chapter3/3_tags.spec.ts
@@ -0,0 +1,69 @@
+import {test,expect} from '@playwright/test'
+
+
+test("Validating the dropdown",{tag:['@Smoketesting']},async({page})=>{
+
+   await page.goto("https://jqueryui.com/droppable/")
+
+   // if the locators are under iframe, then first we have to locate iframe and store it 
+
+   const iframe = page.frameLocator("[class='demo-frame']")
+
+   // Now the item you want to drag
+
+   const drag = iframe.locator("[id='draggable']")
+
+   // Now the item where you have to drop
+
+   const drop = iframe.locator("[id='droppable']")
+
+   // now dragging
+
+   await drag.dragTo(drop)
+})
+
+
+
+test("Validating the dropdown2",{tag:['@Smoketesting','@RegressionTesting']},async({page})=>{
+
+   await page.goto("https://jqueryui.com/droppable/")
+
+   // if the locators are under iframe, then first we have to locate iframe and store it 
+
+   const iframe = page.frameLocator("[class='demo-frame']")
+
+   // Now the item you want to drag
+
+   const drag = iframe.locator("[id='draggable']")
+
+   // Now the item where you have to drop
+
+   const drop = iframe.locator("[id='droppable']")
+
+   // now dragging
+
+   await drag.dragTo(drop)
+})
+
+
+
+test("Validating the dropdown3",{tag:['@RegressionTesting']},async({page})=>{
+
+   await page.goto("https://jqueryui.com/droppable/")
+
+   // if the locators are under iframe, then first we have to locate iframe and store it 
+
+   const iframe = page.frameLocator("[class='demo-frame']")
+
+   // Now the item you want to drag
+
+   const drag = iframe.locator("[id='draggable']")
+
+   // Now the item where you have to drop
+
+   const drop = iframe.locator("[id='droppable']")
+
+   // now dragging
+
+   await drag.dragTo(drop)
+})
