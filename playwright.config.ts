017e03b47b0f33ec2064815d305574d0bafbfa5c
Aditya-1406 <gargaditya674@gmail.com>
2025-12-24 10:45:35 +0530

    video recording

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     screenshot:'on',
+    video:'on',
     testIdAttribute : "data-date",
     trace: 'on',
     headless: false, 
